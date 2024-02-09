b771111b047822c6d070cbf1974c1749ae4ad878
muzzammil763 <deadbase763@gmail.com>
2024-02-09 16:28:37 +0500

    added some components in sign in screen & appbar removed from screen selecter ...

--- lib/Screens/screen_selecter.dart
+++ lib/Screens/screen_selecter.dart
@@ -1,81 +1,58 @@
 import 'package:event_planner_ui/Screens/signin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ScreenSelecter extends StatelessWidget {
   const ScreenSelecter({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(24),
-          ),
-        ),
-        backgroundColor: Colors.lightGreen,
-        title: const Text(
-          "Select Any Screen",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 19,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(
-              right: 16,
-            ),
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.restart_alt_rounded,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
+    return AnnotatedRegion(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          left: 24,
-          right: 24,
-        ),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 8,
-            ),
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SigninScreen(),
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(
+            left: 24,
+            right: 24,
+            top: 72,
+          ),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 8,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SigninScreen(),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 60,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent[700],
+                    borderRadius: BorderRadius.circular(24),
                   ),
-                );
-              },
-              child: Container(
-                height: 60,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.blueAccent[700],
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                child: const Center(
-                  child: Text(
-                    "Sign in",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+                  child: const Center(
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/Screens/signin.dart
+++ lib/Screens/signin.dart
@@ -1,13 +1,152 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SigninScreen extends StatelessWidget {
+class SigninScreen extends StatefulWidget {
   const SigninScreen({super.key});
 
   @override
+  State<SigninScreen> createState() => _SigninScreenState();
+}
+
+class _SigninScreenState extends State<SigninScreen> {
+  bool passwordVisible = false;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Sign In"),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 24,
+          right: 24,
+          top: 72,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 80,
+              width: 80,
+              child: Image.asset(
+                "assets/images/logo.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+            const Text(
+              "EventHub",
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Sign in",
+                  style: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextField(
+              style: const TextStyle(
+                fontSize: 15,
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                ),
+                prefixIcon: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 16,
+                  ),
+                  child: Icon(
+                    CupertinoIcons.mail,
+                    color: Colors.black45,
+                  ),
+                ),
+                hintText: 'abc@gmail.com',
+                hintStyle: const TextStyle(
+                  color: Colors.black38,
+                  fontSize: 13,
+                ),
+                filled: true,
+                fillColor: const Color(0XFFF3F4F6),
+              ),
+              textInputAction: TextInputAction.next,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextField(
+              style: const TextStyle(
+                fontSize: 15,
+                color: Colors.black,
+              ),
+              obscureText: passwordVisible,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                ),
+                hintText: "Password",
+                hintStyle: const TextStyle(
+                  color: Colors.black38,
+                  fontSize: 13,
+                ),
+                prefixIcon: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 16,
+                  ),
+                  child: Icon(
+                    CupertinoIcons.lock,
+                    color: Colors.black45,
+                  ),
+                ),
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.only(
+                    right: 8,
+                  ),
+                  child: IconButton(
+                    icon: Icon(
+                      passwordVisible
+                          ? Icons.visibility_outlined
+                          : Icons.visibility_off_outlined,
+                      size: 25,
+                      color: Colors.black38,
+                    ),
+                    onPressed: () {
+                      setState(
+                        () {
+                          passwordVisible = !passwordVisible;
+                        },
+                      );
+                    },
+                  ),
+                ),
+                filled: true,
+                fillColor: const Color(0XFFF3F4F6),
+              ),
+              keyboardType: TextInputType.visiblePassword,
+              textInputAction: TextInputAction.done,
+            ),
+          ],
+        ),
       ),
     );
   }
