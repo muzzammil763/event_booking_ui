afadaddf44de763030b5e6fc25da2be30a809a8f
muzzammil763 <deadbase763@gmail.com>
2024-02-09 15:46:45 +0500

    added sign in screen ..

--- lib/Screens/screen_selecter.dart
+++ lib/Screens/screen_selecter.dart
@@ -1,3 +1,4 @@
+import 'package:event_planner_ui/Screens/signin.dart';
 import 'package:flutter/material.dart';
 
 class ScreenSelecter extends StatelessWidget {
@@ -7,8 +8,74 @@ class ScreenSelecter extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Text",
+        centerTitle: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(24),
+          ),
+        ),
+        backgroundColor: Colors.lightGreen,
+        title: const Text(
+          "Select Any Screen",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 19,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 16,
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.restart_alt_rounded,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 24,
+          right: 24,
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 8,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SigninScreen(),
+                  ),
+                );
+              },
+              child: Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent[700],
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: const Center(
+                  child: Text(
+                    "Sign in",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
