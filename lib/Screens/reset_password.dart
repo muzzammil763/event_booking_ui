7f97c3449f465967b269ac90b3c5b1024f73e48e
muzzammil763 <deadbase763@gmail.com>
2024-02-10 14:38:10 +0500

    added the complete ui of reset password screen

--- lib/Screens/reset_password.dart
+++ lib/Screens/reset_password.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ResetPassword extends StatelessWidget {
@@ -5,9 +6,137 @@ class ResetPassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Reset Password"),
+    return Scaffold(
+      backgroundColor: const Color(0XFFF3F4F6),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 24,
+          right: 24,
+          top: 72,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(
+                  CupertinoIcons.back,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Reset Password",
+                  style: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Please enter your email address to\nrequest a password reset",
+                  style: TextStyle(
+                    color: Colors.black54,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            TextField(
+              style: const TextStyle(
+                fontSize: 15,
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                ),
+                prefixIcon: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 16,
+                  ),
+                  child: Icon(
+                    CupertinoIcons.mail,
+                    color: Colors.black45,
+                  ),
+                ),
+                hintText: 'abc@gmail.com',
+                hintStyle: const TextStyle(
+                  color: Colors.black38,
+                  fontSize: 13,
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              textInputAction: TextInputAction.next,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 64,
+              ),
+              child: Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent[700],
+                  borderRadius: BorderRadius.circular(32),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 16,
+                    ),
+                    //icon for space settlment
+                    Icon(
+                      Icons.arrow_forward,
+                      color: Colors.transparent,
+                    ),
+                    //icon for space settlment
+                    Spacer(),
+                    Text(
+                      "Send",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+                    Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
