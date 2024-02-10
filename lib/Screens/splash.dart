6fdf24ebbd1c967b936885ca729e4367f88f32f8
muzzammil763 <deadbase763@gmail.com>
2024-02-10 15:06:27 +0500

    added splash screen ui .

--- lib/Screens/splash.dart
+++ lib/Screens/splash.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Splash Screen"),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 80,
+              width: 80,
+              child: Image.asset(
+                "assets/images/logo.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text(
+              "vent",
+              style: GoogleFonts.bebasNeue(
+                fontSize: 40,
+                fontWeight: FontWeight.w900,
+                color: Colors.blueAccent[700],
+              ),
+            ),
+            Text(
+              "Hub",
+              style: GoogleFonts.poppins(
+                fontSize: 40,
+                fontWeight: FontWeight.w900,
+                color: const Color(0XFF00F8FF),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
