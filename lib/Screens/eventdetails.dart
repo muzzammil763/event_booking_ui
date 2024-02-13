9c2450e5ce8fdd0bb4cde9b16c6b2a66d777c6cd
muzzammil763 <deadbase763@gmail.com>
2024-02-13 20:57:33 +0500

    added components in event details screen ...

--- lib/Screens/eventdetails.dart
+++ lib/Screens/eventdetails.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EventDetailsScreen extends StatelessWidget {
@@ -5,9 +6,57 @@ class EventDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Event Details"),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.blueAccent[700],
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(48),
+                bottomRight: Radius.circular(48),
+              ),
+            ),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    CupertinoIcons.arrow_left,
+                    color: Colors.white,
+                  ),
+                ),
+                const Text(
+                  "Event Details",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 23,
+                  ),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    CupertinoIcons.bookmark_solid,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Container(
+            height: 60,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.blueAccent[700],
+              borderRadius: BorderRadius.circular(32),
+            ),
+          )
+        ],
       ),
     );
   }
