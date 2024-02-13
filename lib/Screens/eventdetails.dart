ae4c4ed3405ad8ba928449648a873e944278f529
muzzammil763 <deadbase763@gmail.com>
2024-02-13 21:43:24 +0500

    added more components in detail screen ...

--- lib/Screens/eventdetails.dart
+++ lib/Screens/eventdetails.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EventDetailsScreen extends StatelessWidget {
   const EventDetailsScreen({super.key});
@@ -18,31 +19,67 @@ class EventDetailsScreen extends StatelessWidget {
                 bottomRight: Radius.circular(48),
               ),
             ),
-            child: Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    CupertinoIcons.arrow_left,
-                    color: Colors.white,
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 48,
                   ),
-                ),
-                const Text(
-                  "Event Details",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 23,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            CupertinoIcons.arrow_left,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const Text(
+                          "Event Details",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1.2,
+                          ),
+                        ),
+                        const Spacer(),
+                        Container(
+                          height: 36,
+                          width: 36,
+                          decoration: BoxDecoration(
+                            color: Colors.white30,
+                            borderRadius: BorderRadius.circular(8),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 10,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                CupertinoIcons.bookmark_solid,
+                                color: Colors.white,
+                                size: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const Spacer(),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    CupertinoIcons.bookmark_solid,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           const SizedBox(
@@ -52,10 +89,220 @@ class EventDetailsScreen extends StatelessWidget {
             height: 60,
             width: 300,
             decoration: BoxDecoration(
-              color: Colors.blueAccent[700],
+              color: Colors.grey[300],
               borderRadius: BorderRadius.circular(32),
             ),
-          )
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              child: Row(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Stack(
+                        children: [
+                          const SizedBox(
+                            height: 36,
+                            width: 88,
+                          ),
+                          Positioned(
+                            child: CircleAvatar(
+                              radius: 18,
+                              child: Image.asset(
+                                "assets/images/facebook.png",
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            right: 26,
+                            child: CircleAvatar(
+                              radius: 18,
+                              child: Image.asset(
+                                "assets/images/facebook.png",
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            right: 0,
+                            child: CircleAvatar(
+                              radius: 18,
+                              child: Image.asset("assets/images/facebook.png"),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        "+20 Going",
+                        style: GoogleFonts.lexend(
+                          color: Colors.blueAccent[700],
+                          letterSpacing: 0,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  Container(
+                    height: 30,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent[700],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "INVITE",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 11,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 24,
+            ),
+            child: Text(
+              "International Band Music Concert",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 24,
+            ),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      CupertinoIcons.calendar,
+                      color: Colors.blueAccent[700],
+                      size: 30,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "14 December, 2021",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "Tuesday, 4:00PM - 9:00PM",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black45,
+                        letterSpacing: 0,
+                        wordSpacing: -2,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 24,
+            ),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      CupertinoIcons.location_solid,
+                      color: Colors.blueAccent[700],
+                      size: 30,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Gala Convention Center",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "36 Guild Street London, UK",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black45,
+                        letterSpacing: 0,
+                        wordSpacing: -2,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
